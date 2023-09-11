28dd3da2a191304d52673cee2bd86cffd018ee3f
Jonas Lara <adrianlr2515@outlook.com>
2023-09-10 22:37:45 -0600

    UPDATE

--- Setup.sh
+++ Setup.sh
@@ -5,33 +5,46 @@ echo " "
 
 sudo apt update 
 
-sudo apt install curl neovim tmux -y
+sudo apt install curl vim tmux libuv1-dev -y
 
+echo " "
 echo "Install oh my bash"
 echo " "
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+echo " "
 echo "Install vim plug"
 echo " "
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim    
     
-    
-echo "Install FSharp autocomplete"
+echo " "
+echo "Install fsharp autocomplete"
 echo " "
 
 dotnet tool install -g fsautocomplete
 
+echo " "
+echo "Configuring the clipboard in vim"
+echo " "
+
+./CopyPasteVim.sh
+
+echo " "
+echo "Configuring .tmux.conf and .vimrc"
 echo " "
 echo "$(cat Sources/_tmux.conf)" |  sudo tee  ~/.tmux.conf
 
 # reload config file (change file location to your the tmux.conf you want to use)
 bind r source-file ~/.tmux.conf
 
-mkdir ~/.config/nvim && touch ~/.config/nvim/init.vim 
+echo " "
+echo "$(cat Sources/_vimrc)" | sudo tee ~/.vimrc
+echo " "
 
 echo " "
-echo "$(cat Sources/_vimrc)" | sudo tee ~/.config/nvim/init.vim
+echo "Your vim and tmux has been successfully configured"
 echo " "
+
---
+++ Sources/CopyPasteVim.sh
@@ -0,0 +1,17 @@
+vim --version | grep clipboard
+
+sudo apt-get install vim-gtk3
+
+sudo apt-get purge vim-common
+
+sudo apt install vim
+
+sudo apt-get purge vim-runtime
+
+sudo apt install vim
+
+sudo apt install vim-gtk
+
+
+
+
